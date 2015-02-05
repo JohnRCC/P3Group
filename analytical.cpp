7b393d3b9ff5c6902419292d4b043b36abe9b610
miktitor <viktor.manuel.nordgren@gmail.com>
2015-02-05 00:19:49 +0000

    05/02-15 00:19 - Analytical seems to be working, comes with its own plot-file

--- analytical.cpp
+++ analytical.cpp
@@ -6,13 +6,16 @@
 using namespace std;
 
 //function prototypes
-float potential(float x,float y,float r);
+float potential(float x,float y,float r,float Plate_separation);
 float cf(float matind,float min,float ds); 
 
 
 int main(int argc, char* argv[])
 {
 	
+/*
+ * 	Verifying and casting correct amount of commandline arguments
+ */	
 if(argc<6) {
   cout<<"Usage: "<<argv[0]<<" [Min x/y val][Max x/y val][x/y divisions (ds)][Circle Radius][No. Iterations][Output type]\n";
 return 1;
@@ -25,30 +28,46 @@ float r=strtod(argv[4],NULL);
 int iter=strtod(argv[5],NULL);
 int index=strtod(argv[6],NULL);
   
-//get matrix size
-int matsize = (((float)smax-smin)/ds) - fmod((smax-smin)/ds,1); 
+int row,column,i;
+      
+/*
+ *	GENERATE MATRIX
+ *
+ *		Sets the values as defined by the parameters passed such that
+ *		 System A's setting is satisfied: 
+ *		 	- Plates of opposite polarities at either side horiz.
+ *		 	- Ground, circular plate positioned at middle
+ */
 
-//initalise matrix
+// Initalise matrix
+int matsize = (((float)smax-smin)/ds) - fmod((smax-smin)/ds,1); 
 float mid = (matsize/2.0) - (fmod(matsize,2.0));
 double valsA[matsize][matsize]; 
-	      
-//generate matrix
-for(int row=0;row<matsize;row++) {
-  for(int column=0;column<matsize;column++) {
+
+// Useful quantities
+//
+// 	Plate separation: (system span) - 2(matrix cell size)
+float d=(smax-smin)-2*ds;
+
+// Cast values
+for(row=0;row<matsize;row++) {
+  for(column=0;column<matsize;column++) {
+	// Sets "true" cordinets for x and y
     float x=cf(row,smin,ds);
     float y=cf(column,smin,ds);
+
         if(column==0) {
-      vals[row][column][0]=vals[row][column][1]=1;
+      valsA[row][column]=valsA[row][column]=1;
       }
       else if(column==matsize-1) {
-	vals[row][column][0]=vals[row][column][1]=-1;
+	valsA[row][column]=valsA[row][column]=-1;
       }
       else if( (pow((cf(row,smin,ds)-cf(mid,smin,ds)),2.0) + pow((cf(column,smin,ds)-cf(mid,smin,ds)),2.0)) < pow(r,2.0) ) {
-	vals[row][column][0]=vals[row][column][1]=0;
+	valsA[row][column]=valsA[row][column]=0;
       }
       else {
       	// Only values that differ from main
-	vals[row][column][0]=vals[row][column][1]=potential(x,y,r);
+	valsA[row][column]=valsA[row][column]=potential(x,y,r,d);
       }
 	
   }
@@ -70,22 +89,27 @@ for(int row=0;row<matsize;row++) {
 */
 
 
-datafile.open("Analytic.dat")
+/*
+ *	Writing data to file
+ */
+
+ofstream datafile;
+datafile.open("analytical.dat");
 
 for(row=0;row<matsize;row++) {
 	for(column=0;column<matsize;column++) {
     //cout<<"File Iter: "<<row<<"\r";
     if(index==0){
       //gradient test. see function 'grad' for more info.
-      datafile<<cf(row,smin,ds)<<" "<<cf(column,smin,ds)<<" "<<vals[row][column]<<"\n";
+      datafile<<cf(row,smin,ds)<<" "<<cf(column,smin,ds)<<" "<<valsA[row][column]<<"\n";
     }
     else if(index==1){
       //actual values of potential (for plotting etc.)
-      datafile<<row<<" "<<column<<" "<<vals[row][column]<<"\n";
+      datafile<<row<<" "<<column<<" "<<valsA[row][column]<<"\n";
     }
     else if(index==2){
       //display values in terminal
-      cout<<vals[row][column]<<" ";
+      cout<<valsA[row][column]<<" ";
     }
   }
   //cout<<"\n";
@@ -98,11 +122,9 @@ datafile.close();
 
 }
 
-float potential(float x, float y, float r)
-{
-  float v=0, Plate_Vdiff=2, Plate_separation=r*pow(10,2);
+float potential(float x, float y, float r,float Plate_separation) {
+  float v=0, Plate_Vdiff=2; //Plate_separation=r*pow(10,2);
   //float e0=8.85*pow(10,-12); // permitivity of free space
-
 
   // E = sigma/e0
   // float sigma=1;
---
+++ plot_analytical.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+gnuplot << EOF
+set term postscript
+set output "analytical_plot.eps"
+# set dgrid3d
+# set parametric
+ set contour base
+ set view map
+# set view 0,0,1
+unset surface
+set title ""
+ set cntrparam levels 200
+splot "analytical.dat" using 2:1:3 with image notitle	# line for contour plot
+EOF
+
