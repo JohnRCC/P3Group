c3a3bfc91415a17dd435fa66b65a1706499840da
John Cork <johnrcc@jrccrunch.(none)>
2015-02-12 14:34:46 +0000

    2015/02/12 14:34 Field lines now display between 1 and 0

---
+++ electric.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [[ $# -ge 2 ]] ; then
+    ./main
+else
+    echo "Welcome to Electric! Please choose an option:"
+    echo "  [1] Image Mode"
+    echo "  [2] Analytic Mode"
+    echo "  [3] Plotting Mode"
+
+
+
+
+fi
+exit 0
