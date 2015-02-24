0a7b4b78f5bb37dd0b2c13965bb20106e88b6f5b
John Cork <2022533c@student.gla.ac.uk>
2015-02-24 14:56:30 +0000

    numerical image mode works now in shell script. next up: plotting

--- electric2.sh
+++ electric2.sh
@@ -44,127 +44,208 @@ while [ $run = "yes" ] ; do
 
 	# Case for image / analytical modes (choosing output file first)
 	if [[ $choice == "i" || $choice == "a" ]] ; then
+
+	    checkloop=yes #loop in case user wants to re-enter variables
+	    while [ $checkloop = "yes" ] ; do
 	    
 	    # Gets rid of old pot.dat, field.dat, grad.dat files if present (they shouldn't be)
-	    if [[ -f pot.dat ]] ; then
-		rm pot.dat
-	    fi
-
-	    if [[ -f field.dat ]] ; then
-		rm field.dat
-	    fi
-
-	    if [[ -f grad.dat ]] ; then
-		rm grad.dat
-	    fi
-	    
+		if [[ -f pot.dat ]] ; then
+		    rm pot.dat
+		fi
+		
+		if [[ -f field.dat ]] ; then
+		    rm field.dat
+		fi
+		
+		if [[ -f grad.dat ]] ; then
+		    rm grad.dat
+		fi
+		
 	    # Gives the correct name based on choice
-	    if [[ $choice == "i" ]] ; then
-		choicename=Image
-	    elif [[ $choice == "a" ]] ; then
-		choicename=Analytic
-	    fi
-	    
+		if [[ $choice == "i" ]] ; then
+		    choicename=Image
+		elif [[ $choice == "a" ]] ; then
+		    choicename=Analytic
+		fi
+		
 	    # File selector. If file already exists, will keep old files the same and add a number to the new one.
-	    echo $choicename "mode selected. Please name your output file (will be used for potential, field and grad if requested):"
-	    read outfile ; clear
-	    
-	    if [[ -f ./data/pot/$outfile.pot || -f ./data/fld/$outfile.fld || -f ./data/grd/$outfile.grd ]] ; then
+		echo $choicename "mode selected. Please name your output file (will be used for potential, field and grad if requested):"
+		read outfile ; clear
 		
-		fileloop=yes # Maintains while loop until unused filename is found.
-		fileiter=0
-		
-		echo "File(s) "$outfile" already present."
-		
-		while [ $fileloop == "yes" ] ; do
+		if [[ -f ./data/pot/$outfile.pot || -f ./data/fld/$outfile.fld || -f ./data/grd/$outfile.grd ]] ; then
 		    
-		    fileiter=$(( $fileiter + 1 ))
+		    fileloop=yes # Maintains while loop until unused filename is found.
+		    fileiter=0
 		    
+		    echo "File(s) "$outfile" already present."
+		    
+		    while [ $fileloop == "yes" ] ; do
+			
+			fileiter=$(( $fileiter + 1 ))
+			
 		    # in first case, check if any numbered versions of file exist
 		#   if [[ ! -f ./data/pot/$outfile\_1.pot || ! -f ./data/fld/$outfile\_1.fld || ! -f ./data/grd/$outfile\_1.grd ]] ; then
 		#	outfile=$outfile\_1
 		#	fileloop=no
-		    if [[ ! -f ./data/pot/$outfile\_$fileiter.pot || ! -f ./data/fld/$outfile\_$fileiter.fld || ! -f ./data/grd/$outfile\_$fileiter.grd ]] ; then
-			outfile=$outfile\_$fileiter
-			fileloop=no
-		    fi
-		done
+			if [[ ! -f ./data/pot/$outfile\_$fileiter.pot || ! -f ./data/fld/$outfile\_$fileiter.fld || ! -f ./data/grd/$outfile\_$fileiter.grd ]] ; then
+			    outfile=$outfile\_$fileiter
+			    fileloop=no
+			fi
+		    done
+		    
+		    echo "Renaming to "$outfile"."
+		    sleep 1
+		fi
 		
-		echo "Renaming to "$outfile"."
-	    fi
-	    
 	    # Initialise variable for generating different files. (needed in both cases)
-	    option=0
-
+		option=0
+		
 	    # Now branch off into image or analytic mode
-	    if [[ $choice == "i" ]] ; then
-		
-		clear
-
-		echo "Please enter image file name (with extension):"
-		read imagefile ; clear
-
-		# Validity check on filename
-		while [ ! -f $imagefile ] ; do
-		    echo "Invalid image specified. Please try again:"
+		if [[ $choice == "i" ]] ; then
+		    
+		#   clear
+		    
+		    echo "Please enter image file name (with extension):"
 		    read imagefile ; clear
-		done
-
-		echo $imagefile" selected. Calculate equipotentials? [y/n]"
-		read pot ; clear
-
-		while [ $pot != "y" ] && [ $pot != "n" ] ; do
-		    echo "Please type y or n:"
+		    
+		# Validity check on filename
+		    while [ ! -f $imagefile ] ; do
+			echo "Invalid image specified. Please try again:"
+			read imagefile ; clear
+		    done
+		    
+		    echo $imagefile" selected. Calculate equipotentials? [y/n]"
 		    read pot ; clear
-		done
-
-		if [[ $pot == "y" ]] ; then
-		    option=$(( $option + 2 ))
-		    echo "Calculating equipotentials. Calculate electric field? [y/n]"
-		elif [[ $pot == "n" ]] ; then
-		    touch ./data/pot/$outfile.pot # to maintain symmetry in filenames
-		    echo "Not calculating equipotentials. Calculate electric field? [y/n]"
-		fi
-
-		read fld ; clear
-
-		while [ $fld != "y" ] && [ $fld != "n" ] ; do
-		    echo "Please type y or n:"
+		    
+		    while [ $pot != "y" ] && [ $pot != "n" ] ; do
+			echo "Please type y or n:"
+			read pot ; clear
+		    done
+		    
+		    if [[ $pot == "y" ]] ; then
+			option=$(( $option + 2 ))
+			echo "Calculating equipotentials. Calculate electric field? [y/n]"
+		    elif [[ $pot == "n" ]] ; then
+		#	touch ./data/pot/$outfile.pot # to maintain symmetry in filenames
+			echo "Not calculating equipotentials. Calculate electric field? [y/n]"
+		    fi
+		    
 		    read fld ; clear
-		done
+		    
+		    while [ $fld != "y" ] && [ $fld != "n" ] ; do
+			echo "Please type y or n:"
+			read fld ; clear
+		    done
+		    
+		    if [[ $fld == "y" ]] ; then
+			option=$(( $option + 4 ))
+			echo "Calculating electric field. Output grad test results? [y/n] (Advanced users only: recommended choice no)"
+		    elif [[ $fld == "n" ]] ; then
+		#	touch ./data/fld/$outfile.fld
+			echo "Not calculating electric field. Output grad test results? [y/n] (Advanced users only: recommended choice no)"
+		    fi
+		    
+		    read grd ; clear
+		    
+		    while [ $grd != "y" ] && [ $grd != "n" ] ; do
+			echo "Please type y or n:"
+			read grd; clear
+		    done
+		    
+		    if [[ $grd == "y" ]] ; then
+			option=$(( $option + 1 ))
+			echo "Calculating grad test results. Error tolerance? (Used to determine number of iterations, a good guideline number is ____."
+		    elif [[ $grd == "n" ]] ; then
+		#	touch ./data/grd/$outfile.grd
+			echo "Not calculating grad test results. Error tolerance? (Used to determine number of iterations, a good guideline number is ____."
+		    fi
+		    
+		    read errtol ; clear
+		    
+		    echo "Apply smoothing? [y/n]"
+		    read smooth ; clear
+		    
+		    while [ $smooth != "y" ] && [ $smooth != "n" ] ; do
+			echo "Please type y or n:"
+			read smooth ; clear
+		    done
+		    
+		    if [[ $smooth == "y" ]] ; then
+			smoothnum=1
+		    else
+			smoothnum=0
+			smooth=n
+		    fi
+		    
+		    echo "Here are your settings. Type 'run' to begin or 'reset' to re-enter values."
+		    echo "             Output: "$outfile
+		    echo "         Image file: "$imagefile
+		    echo "Calculate potential: "$pot
+		    echo "    Calculate field: "$fld
+		    echo " Calculate gradient: "$grd
+		    echo "    Error tolerance: "$errtol
+		    echo "          Smoothing: "$smooth
+		    
+		    read go ; clear
 
-		if [[ $fld == "y" ]] ; then
-		    option=$(( $option + 4 ))
-		    echo "Calculating electric field. Output grad test results? [y/n] (Advanced users only: recommended choice no)"
-		elif [[ $fld == "n" ]] ; then
-		    touch ./data/fld/$outfile.fld
-		    echo "Not calculating electric field. Output grad test results? [y/n] (Advanced users only: recommended choice no)"
+		    while [ $go != "run" ] && [ $go != "reset" ] ; do
+			echo "Please type 'run' or 'reset':"
+			read go ; clear
+		    done
+
+		    if [[ $go = "run" ]] ; then
+			checkloop=no
+			./main $imagefile $errtol $option $smooth
+
+			#move generated files into respective folders
+			if [[ -f pot.dat ]] ; then
+			    mv pot.dat ./data/pot/$outfile.pot
+			else
+			    cp ./data/pot/.empty.pot ./data/pot/$outfile.pot
+			fi
+
+			if [[ -f field.dat ]] ; then
+			    mv field.dat ./data/fld/$outfile.fld
+			else
+			    cp ./data/fld/.empty.fld ./data/fld/$outfile.fld
+			fi
+
+			if [[ -f grad.dat ]] ; then
+			    mv grad.dat ./data/grd/$outfile.grd
+			else
+			    cp ./data/grd/.empty.grd ./data/grd/$outfile.grd
+			fi
+
+			echo "Calculations complete. Go to plotting mode now? [y/n]"
+			read plotq ; clear
+
+			while [ $plotq != "y" ] && [ $plotq != "n" ] ; do
+			    echo "Please enter y or n:"
+			    read plotq ; clear
+			done
+
+			if [[ $plotq = "y" ]] ; then
+			    choice=p
+			fi
+		    elif [[ $go = "reset" ]] ; then
+			echo "Resetting..."
+		    fi
+		    
+		    
 		fi
-
-		read grd ; clear
 		
-		while [ $grd != "y" ] && [ $grd != "n" ] ; do
-		    echo "Please type y or n:"
-		    read grd; clear
-		done
-
-		if [[ $grd == "y" ]] ; then
-		    option=$(( $option + 1 ))
-		    echo "Calculating grad test results. Error tolerance? (Used to determine number of iterations, a good guideline number is ____."
-		elif [[ $grd == "n" ]] ; then
-		    touch ./data/grd/$outfile.grd
-		    echo "Not calculating grad test results. Error tolerance? (Used to determine number of iterations, a good guideline number is ____."
+		if [[ $choice == "a" ]] ; then
+		    echo analysis
+		    echo "woo" > ./data/pot/$outfile.pot
+		    echo "yay" > ./data/fld/$outfile.fld
+		    echo "gud" > ./data/grd/$outfile.grd
 		fi
+	    done
+	fi
 
-		echo "HERE WE GOOOOOOOO" 
-	    fi
-	    
-	    if [[ $choice == "a" ]] ; then
-		echo anal
-		echo "woo" > ./data/pot/$outfile.pot
-		echo "yay" > ./data/fld/$outfile.fld
-		echo "gud" > ./data/grd/$outfile.grd
-	    fi
+	#plotting mode
+	if [[ $choice = "p" ]] ; then
+	    echo "Entering plotting mode. Please enter filename you want to plot (without extension):"
 	fi
     fi
 done
