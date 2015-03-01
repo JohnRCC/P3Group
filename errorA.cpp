8d542ac9edc427c53f762952374e860d5e5d1a58
John Cork <2022533c@student.gla.ac.uk>
2015-03-01 21:25:51 +0000

    2015-03-01 21:25 - Improved readability of algorithm, updated electric.sh

--- errorA.cpp
+++ errorA.cpp
@@ -30,25 +30,35 @@ int main(int argc, char* argv[])
 
 	float confidence, minx, maxx, radius, distance; 
 
-	confidence = strtof(argv[1],NULL); 
+	//confidence = strtof(argv[1],NULL);
+	confidence = 90;
 	minx = strtof(argv[2],NULL);
 	maxx = strtof(argv[3],NULL);
 	distance = maxx-minx;
 	radius = strtof(argv[4],NULL);
+	
+	float recommended = (2*radius)/cbrt(0.1);
 
 	float cubeconf = cbrt((100-confidence)/100);
-
+	
 	float ratio = (2*radius)/distance;
 
 	if (ratio > cubeconf){
 
-		cout << "cannot give " << confidence << 
+		/*cout << "cannot give " << confidence << 
 		"\% confidence with these dimensions;\nmake radius " <<
-		"at most " << (distance/2)*cubeconf << endl;
+		"at most " << (distance/2)*cubeconf << endl;*/
+		
+		cout<<"Warning: dimensions specified unlikely to give accurate results (less than "<<confidence<<"\% confidence)."<<endl;
+		cout<<"Type 'r' to re-define values, or 'y' to proceed. (A good plate separation for R="<<radius<<" is "<<int(recommended+1)<<".)"<<endl;
+		
 		return 1;
 	}
 	else{
-		cout << "zero!\n";	
+		//cout << "zero!\n";	
+		cout<<"Dimensions specified will give accurate results with "<<100*(1-pow(ratio,3))<<"\% confidence."<<endl;
+		cout<<"Type 'r' to redefine these values, or 'y' to proceed."<<endl;
+		
 		return 0;
 	}
 }
